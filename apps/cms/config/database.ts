f2857af98022bd84d1a8f8dafbfdb6ebe042c0cb
DrowningToast <supratouchgussii@gmail.com>
2022-10-08 17:49:38 +0700

    feat: strapi docker compose

---
+++ apps/cms/config/database.ts
@@ -0,0 +1,13 @@
+export default ({ env }) => ({
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env("DATABASE_HOST", "0.0.0.0"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "tobeit-cms"),
+      user: env("DATABASE_USERNAME", "tobeit"),
+      password: env("DATABASE_PASSWORD", "tobeit"),
+      ssl: env.bool("DATABASE_SSL", false),
+    },
+  },
+});
