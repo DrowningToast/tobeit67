5a5aad75c90b53e42dbb97205a218888acc0163a
DrowningToast <supratouchgussii@gmail.com>
2022-10-08 17:49:46 +0700

    feat: strapi docker compose

--- apps/cms/config/server.ts
+++ apps/cms/config/server.ts
@@ -1,6 +1,6 @@
-module.exports = ({ env }) => ({
+export default ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 5000),
+  port: env.int("PORT", 1337),
   app: {
     keys: env.array("APP_KEYS"),
   },
