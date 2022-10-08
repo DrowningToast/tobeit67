8f642040a473c4e88b1588924ac0c466d64d5092
DrowningToast <supratouchgussii@gmail.com>
2022-10-08 17:03:55 +0700

    refact: strapi to docker

--- apps/cms/config/database.js
+++ apps/cms/config/database.js
@@ -1,11 +1,37 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = ({ env }) => ({
+  // connection: {
+  //   client: "sqlite",
+  //   connection: {
+  //     filename: path.join(
+  //       __dirname,
+  //       "..",
+  //       env("DATABASE_FILENAME", ".tmp/data.db")
+  //     ),
+  //   },
+  //   useNullAsDefault: true,
+  // },
   connection: {
-    client: 'sqlite',
+    client: "postgres",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: "localhost",
+      port: env.int("POSTGRES_SERVER_PORT", 5432),
+      database: env("POSTGRES_CMS_DB_NAME", "tobeit-cms"),
+      user: env("POSTGRES_USERNAME", "tobeit"),
+      password: env("POSTGRES_PASSWORD", "tobeit"),
+      ssl: env.bool("DATABASE_SSL", false),
     },
-    useNullAsDefault: true,
+    // connection: {
+    //   host: "localhost",
+    //   port: 9876,
+    //   database: "app",
+    //   user: "db_username",
+    //   password: "db_password",
+    //   ssl: env.bool("DATABASE_SSL", false),
+    // },
+    ssl: {
+      rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+    },
   },
 });
