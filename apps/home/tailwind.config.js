dc379e4b7015308a75f5e9ee537d448be5db5c0d
Supratouch <supratouchgussii@gmail.com>
2022-10-10 09:54:42 +0700

    switch: chakra to tailwind

---
+++ apps/home/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{html,js}",
+    "./components/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
