192131826fca586f3ca906e5e85e1e8461ec5d66
DrowningToast <supratouchgussii@gmail.com>
2022-10-11 05:37:54 +0700

    adjust clouds

--- apps/home/components/hero/hero.tsx
+++ apps/home/components/hero/hero.tsx
@@ -1,8 +1,12 @@
-import { motion } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 const Hero = () => {
+  const { scrollY } = useScroll();
+  const dy = useTransform(scrollY, [0, 800], [0, 250]);
+  const opacity = useTransform(scrollY, [0, 300], [0.35, 0.8]);
+
   return (
-    <section className="overflow-x-hidden bg-gradient-to-t from-[#007577] via-[#007577] to-[#FB8763] w-full flex flex-col md:grid md:grid-cols-2 justify-center gap-y-4 px-8 md:px-32 pt-36 md:pt-52 relative">
+    <section className="overflow-x-hidden overflow-y-hidden bg-gradient-to-t from-[#007577] via-[#007577] to-[#FB8763] w-full flex flex-col md:grid md:grid-cols-2 justify-center gap-y-4 px-8 md:px-32 pt-36 md:pt-52 relative">
       {/* Title */}
       <img
         className="self-end md:col-start-2"
@@ -11,9 +15,12 @@ const Hero = () => {
       />
       {/* Clouds A */}
       <motion.img
+        style={{
+          y: dy,
+          opacity,
+        }}
         animate={{
           x: ["0%", "100%"],
-          y: 0,
           transition: {
             ease: "linear",
             duration: 60,
@@ -22,13 +29,16 @@ const Hero = () => {
         }}
         src="/assets/clouds.svg"
         alt="clouds svg"
-        className="absolute top-0 inset-x-0 md:w-full"
+        className="absolute top-6 md:top-0 inset-x-0 md:w-full"
       />
-      {/* Clouds A */}
+      {/* Clouds B */}
       <motion.img
+        style={{
+          y: dy,
+          opacity,
+        }}
         animate={{
           x: ["-100%", "0%"],
-          y: 0,
           transition: {
             ease: "linear",
             duration: 60,
@@ -37,7 +47,7 @@ const Hero = () => {
         }}
         src="/assets/clouds.svg"
         alt="clouds svg"
-        className="absolute top-0 inset-x-0 md:w-full"
+        className="absolute top-6 md:top-0 inset-x-0 md:w-full"
       />
       <button className="md:col-start-2 md:py-3 md:mx-14 bg-glossy-coral font-kanit font-bold text-center inline-block w-full md:w-auto py-2 rounded-full shadow-2xl text-white text-lg tracking-widest">
         ไปลงทะเบียน!
@@ -52,7 +62,7 @@ const Hero = () => {
       <img className="mt-6 z-20 md:hidden" src="/assets/octopus.svg" />
       <img
         src="/assets/octopus-lg.svg"
-        className="z-20 md:row-start-2 md:row-span-3 md:w-full md:scale-125 md:transform md:-translate-y-36 hidden md:block"
+        className="z-20 md:row-start-2 md:row-span-3 md:w-full md:scale-150 md:transform md:-translate-y-36 hidden md:block"
       />
     </section>
   );
