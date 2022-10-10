5c7cde6b31fcce558ea0cee825a806f699b1ab88
DrowningToast <supratouchgussii@gmail.com>
2022-10-11 02:47:36 +0700

    feat: responsive hero

--- apps/home/components/hero/hero.tsx
+++ apps/home/components/hero/hero.tsx
@@ -1,24 +1,33 @@
 const Hero = () => {
   return (
-    <section className="bg-gradient-to-t from-[#007577] via-[#007577] to-[#FB8763] w-full flex flex-col justify-center gap-y-4 px-8 pt-36 relative">
-      {/* TItle */}
-      <img className="self-end" src="/assets/tobeit67.svg" alt="ToBeIT'67" />
+    <section className="bg-gradient-to-t from-[#007577] via-[#007577] to-[#FB8763] w-full flex flex-col md:grid md:grid-cols-2 justify-center gap-y-4 px-8 md:px-32 pt-36 md:pt-52 relative">
+      {/* Title */}
+      <img
+        className="self-end md:col-start-2"
+        src="/assets/tobeit67.svg"
+        alt="ToBeIT'67"
+      />
       {/* Clouds */}
       <img
         src="/assets/clouds.svg"
         alt="clouds svg"
-        className="absolute top-0 inset-x-0"
+        className="absolute top-0 inset-x-0 md:w-full"
       />
-      <button className="bg-glossy-coral font-kanit font-bold text-center inline-block w-full py-2 rounded-full shadow-2xl text-white text-lg tracking-widest">
+      <button className="md:col-start-2 md:py-3 md:mx-14 bg-glossy-coral font-kanit font-bold text-center inline-block w-full md:w-auto py-2 rounded-full shadow-2xl text-white text-lg tracking-widest">
         ไปลงทะเบียน!
       </button>
       <a
         href="#onsite"
-        className="opacity-70 underline text-white text-center font-noto"
+        className="md:col-start-2 opacity-70 underline text-white text-center font-noto cursor-pointer"
       >
         วิธีลงทะเบียนรอบออนไซต์
       </a>
-      <img className="mt-6 z-20" src="/assets/octopus.svg" />
+      {/* Octopus */}
+      <img className="mt-6 z-20 md:hidden" src="/assets/octopus.svg" />
+      <img
+        src="/assets/octopus-lg.svg"
+        className="z-20 md:row-start-2 md:row-span-3 md:w-full md:scale-150 md:transform md:-translate-y-36"
+      />
     </section>
   );
 };
