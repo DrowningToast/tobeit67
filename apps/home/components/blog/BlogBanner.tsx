ca4a5fa09b63b517b93e86a0dff3473eb3f40354
DrowningToast <supratouchgussii@gmail.com>
2022-10-12 03:30:05 +0700

    feat: responsive blog banner

--- apps/home/components/blog/BlogBanner.tsx
+++ apps/home/components/blog/BlogBanner.tsx
@@ -1,15 +1,15 @@
 const BlogBanner = () => {
   return (
-    <section className="bg-gradient-to-b from-[#FB8763] to-[#D54962] pb-28 pt-8 flex flex-col gap-y-6 relative">
+    <section className="bg-gradient-to-b from-[#FB8763] to-[#D54962] pb-28 md:pb-96 pt-8 md:pt-20 flex flex-col gap-y-6 md:gap-y-16 relative">
       <img
         src="/assets/wave-challenger.svg"
         alt="wave challenger svg"
-        className="absolute top-0 transform -translate-y-[99%] left-0"
+        className="absolute top-0 transform -translate-y-[99%] md:-translate-y-[74.9%] left-0 md:scale-y-50"
       />
-      <h1 className="text-white font-chonburi text-4xl text-center ">
+      <h1 className="text-white font-chonburi text-4xl text-center md:text-7xl ">
         Challenger Deep
       </h1>
-      <button className="inline-block p-2 mx-32 text-xl font-chonburi text-glossy-coral bg-white rounded-full shadow-xl">
+      <button className="inline-block p-2 md:px-12 md:py-6 mx-32 md:w-80 md:mx-auto text-xl md:text-4xl font-chonburi text-glossy-coral bg-white rounded-full shadow-xl">
         ไปอ่านเพิ่ม
       </button>
     </section>
