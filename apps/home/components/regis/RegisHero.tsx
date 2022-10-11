4b0060377120eb44272264aa0555aaf5da5aa2c2
DrowningToast <supratouchgussii@gmail.com>
2022-10-12 03:29:52 +0700

    fix: responsive regis

--- apps/home/components/regis/RegisHero.tsx
+++ apps/home/components/regis/RegisHero.tsx
@@ -6,7 +6,7 @@ const RegisHero = () => {
       <img
         src="/assets/wave-regis.svg"
         alt="wave regis svg"
-        className="absolute top-0 transform -translate-y-[99%] md:-translate-y-[75%] scale-y-50"
+        className="absolute top-0 transform -translate-y-[99%] md:-translate-y-[75%] md:scale-y-50"
       />
       <h1 className="font-chonburi text-3xl md:text-6xl text-center text-radial my-6 md:col-span-3">
         ลงทะเบียน
@@ -27,7 +27,7 @@ const RegisHero = () => {
             </a>
           </Link>
         </div>
-        <div className="mx-16 my-8 border-t-2 md:border-t-0 md:border-r-4 h-72 md:mx-auto md:mt-16 md:py-8 border-white"></div>
+        <div className="mx-16 my-8 border-t-2 md:border-t-0 md:border-r-4 md:h-72 md:mx-auto md:mt-16 md:py-8 border-white"></div>
         <div className="flex flex-col justify-center items-center font-chonburi">
           <h3 className="text-2xl md:text-3xl">รอบ</h3>
           <h2 className="text-4xl md:text-6xl">Onsite</h2>
