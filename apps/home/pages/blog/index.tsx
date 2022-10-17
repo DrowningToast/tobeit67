4a97ae209d57dff4a791eea76908c130f4e97713
DrowningToast <supratouchgussii@gmail.com>
2022-10-18 04:01:14 +0700

    fix: incorrect endpoint

--- apps/home/pages/blog/index.tsx
+++ apps/home/pages/blog/index.tsx
@@ -5,7 +5,7 @@ import BlogCard from "../../components/card/BlogCard";
 import { client } from "../../gql/gql-client";
 
 const endpoint =
-  process.env.NODE_ENV === "development"
+  process.env.NODE_ENV == "development"
     ? process.env.NEXT_PUBLIC_CMS_DEV
     : process.env.NEXT_PUBLIC_CMS_PROD;
 
--- apps/regis/src/cms-gql/cms-gql-client.ts
+++ apps/regis/src/cms-gql/cms-gql-client.ts
@@ -1,24 +1,26 @@
 import fetch from 'cross-fetch';
 import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context'
+import { setContext } from '@apollo/client/link/context';
 
 const endpoint =
-  process.env.NODE_ENV === "development"
+  process.env.NODE_ENV.trim() == 'development'
     ? process.env.CMS_DEV
     : process.env.CMS_PROD;
 
-const httpLink = createHttpLink({ uri: endpoint + "/graphql", fetch })
+console.log(endpoint);
+
+const httpLink = createHttpLink({ uri: endpoint + '/graphql', fetch });
 
 const authLink = setContext((_, { headers }) => {
   return {
     ...headers,
     headers: {
-      Authorization: `Bearer ${process.env.BACKEND_CMS_TOKEN}`
-    }
-  }
-})
+      Authorization: `Bearer ${process.env.BACKEND_CMS_TOKEN}`,
+    },
+  };
+});
 
 export const cmsClient = new ApolloClient({
   link: authLink.concat(httpLink),
-  cache: new InMemoryCache()
-})
+  cache: new InMemoryCache(),
+});
