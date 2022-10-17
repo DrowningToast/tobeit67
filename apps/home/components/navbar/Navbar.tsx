e37e98f37f11b5cd3fd94dd12803f040341b9452
DrowningToast <supratouchgussii@gmail.com>
2022-10-18 05:27:26 +0700

    remove: console.log

--- apps/home/components/navbar/Navbar.tsx
+++ apps/home/components/navbar/Navbar.tsx
@@ -1,79 +1,80 @@
-import Link from 'next/link'
-import { motion, useScroll, useSpring, useTransform, useVelocity } from 'framer-motion'
-import { useEffect } from 'react'
+import Link from "next/link";
+import {
+  motion,
+  useScroll,
+  useSpring,
+  useTransform,
+  useVelocity,
+} from "framer-motion";
+import { useEffect } from "react";
 
 const routes = [
   {
-    name: 'เกี่ยวกับค่ายนี้',
-    route: '#about'
+    name: "เกี่ยวกับค่ายนี้",
+    route: "#about",
   },
   {
-    name: 'กำหนดการ',
-    route: '#agenda'
+    name: "กำหนดการ",
+    route: "#agenda",
   },
   {
-    name: 'ลงทะเบียน',
-    route: '#register'
+    name: "ลงทะเบียน",
+    route: "#register",
   },
   {
-    name: 'ออนไซต์',
+    name: "ออนไซต์",
     notAvaliable: true,
-    route: '#'
+    route: "#",
   },
   {
-    name: 'คลังความรู้',
-    route: '/blog'
+    name: "คลังความรู้",
+    route: "/blog",
   },
   {
-    name: 'FAQ',
-    route: '#faq'
-  }
-]
+    name: "FAQ",
+    route: "#faq",
+  },
+];
 
 const Navbar: React.FC = () => {
-  const { scrollY } = useScroll()
+  const { scrollY } = useScroll();
 
-  const velocity = useVelocity(scrollY)
+  const velocity = useVelocity(scrollY);
   const smoothVelocity = useSpring(velocity, {
     damping: 50,
-    stiffness: 200
-  })
+    stiffness: 200,
+  });
 
-  const opacity = useTransform(smoothVelocity, [-300, 0, 300], [0.1, 1, 0.1])
-
-  useEffect(() => {
-    smoothVelocity.onChange(console.log)
-  }, [])
+  const opacity = useTransform(smoothVelocity, [-300, 0, 300], [0.1, 1, 0.1]);
 
   return (
     <motion.nav
       style={{ opacity }}
-      className='fixed top-0 h-32 w-full z-50 hidden md:flex flex-col items-center justify-center gap-2 drop-shadow-lg'>
-      <div className='w-4/5 bg-white py-2 px-6 rounded-full'>
-        <ul className='flex flex-row items-center justify-between px-2 lg:px-12 font-noto text-glossy-coral text-xl lg:text-2xl select-none'>
+      className="fixed top-0 h-32 w-full z-50 hidden md:flex flex-col items-center justify-center gap-2 drop-shadow-lg"
+    >
+      <div className="w-4/5 bg-white py-2 px-6 rounded-full">
+        <ul className="flex flex-row items-center justify-between px-2 lg:px-12 font-noto text-glossy-coral text-xl lg:text-2xl select-none">
           {routes.map((route, index) => {
             if (route.notAvaliable) {
               return (
                 <li key={index}>
-                  <a className='cursor-not-allowed text-gray-300'>
+                  <a className="cursor-not-allowed text-gray-300">
                     {route.name}
                   </a>
                 </li>
-              )
+              );
             }
             return (
               <li key={index}>
-                <a href={route.route}>
-                  {route.name}
-                </a>
+                <a href={route.route}>{route.name}</a>
               </li>
-            )
+            );
           })}
         </ul>
       </div>
-      <hr className='border-2 w-4/5 rounded-full border-white' />
+      <hr className="border-2 w-4/5 rounded-full border-white" />
     </motion.nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
