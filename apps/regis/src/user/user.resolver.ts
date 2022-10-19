a5acfce9916446e1c99babcce20f724585542ceb
Phatsanphon Nakaranurak <phatsanphon2004@outlook.com>
2022-10-19 10:52:55 +0700

    make user exist handler

--- apps/regis/src/user/user.resolver.ts
+++ apps/regis/src/user/user.resolver.ts
@@ -1,4 +1,4 @@
-import { Inject, UseGuards } from '@nestjs/common';
+import { ConflictException, Inject, UseGuards } from '@nestjs/common';
 import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { AuthGuard } from 'src/auth.guard';
 import { User, UserDelete, UserEdit, UserInput } from './user.model';
@@ -10,43 +10,49 @@ export class UserResolver {
   constructor(private userService: UserService) {}
 
   @Query((returns) => [User], { nullable: true })
-  users(
+  async users(
     @Args({ name: 'user', type: () => User, nullable: true })
     user: User,
   ): Promise<User | User[]> {
     if (user) {
-      return this.userService.findOne(user);
+      return await this.userService.findOne(user);
     } else {
-      return this.userService.findAll();
+      return await this.userService.findAll();
     }
   }
 
   @Mutation((returns) => User)
-  insert_user(
+  async insert_user(
     @Args({ name: 'userInput', type: () => UserInput })
     userInput: UserInput,
   ): Promise<User> {
-    return this.userService.create(userInput);
+    const user = await this.userService.findOne({ email: userInput.email })
+
+    if (user) {
+      throw new ConflictException(`User with email: ${user.email} already exist.`)
+    }
+
+    return await this.userService.create(userInput);
   }
 
   @Mutation((returns) => User)
-  edit_user(
+  async edit_user(
     @Args({ name: 'target', type: () => User }) target: Partial<User>,
     @Args({ name: 'update', type: () => UserEdit }) update: Partial<UserEdit>,
   ): Promise<User> {
-    return this.userService.edit(target, update);
+    return await this.userService.edit(target, update);
   }
 
   @Mutation((returns) => User, {
     description: 'Require at least one of the args',
   })
-  remove_user(
+  async remove_user(
     @Args({
       name: 'user',
       type: () => UserDelete,
     })
     user: Partial<UserDelete>,
   ): Promise<User> {
-    return this.userService.remove(user);
+    return await this.userService.remove(user);
   }
 }
