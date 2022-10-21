cd174145bc5decee4cca3ceb08045b1394e2b16d
Phatsanphon Nakaranurak <phatsanphon2004@outlook.com>
2022-10-21 20:33:37 +0700

    make dev mode guard

--- apps/regis/src/app.module.ts
+++ apps/regis/src/app.module.ts
@@ -10,6 +10,7 @@ import { PreauthMiddleware } from './middleware/preauth.middleware.js';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service';
 import { HttpModule } from '@nestjs/axios'
+import { ConfigModule } from '@nestjs/config'
 
 import { join } from 'path';
 import { UserModule } from './user/user.module';
@@ -22,6 +23,10 @@ import { QuizModule } from './quiz/quiz.module';
       debug: process.env.NODE_ENV === 'development',
       playground: true,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+      isGlobal: true
     }),
     UserModule,
     QuizModule,
