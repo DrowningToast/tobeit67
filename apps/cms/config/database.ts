677eef4e1c53a209ec9796c46648adb39b3a1550
DrowningToast <supratouchgussii@gmail.com>
2022-10-23 12:24:23 +0700

    fix: cms prod database path

--- apps/cms/config/database.ts
+++ apps/cms/config/database.ts
@@ -2,7 +2,12 @@ export default ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "0.0.0.0"),
+      host: env(
+        env("NODE_ENV") === "production"
+          ? env("POSTGRES_SERVER_URL_PROD")
+          : env("DATABASE_HOST"),
+        "0.0.0.0"
+      ),
       port: env.int("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME", "tobeit-cms"),
       user: env("DATABASE_USERNAME", "tobeit"),
