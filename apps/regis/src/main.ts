470ad59e9b9b785e42d40b78a65d513d2de00e59
DrowningToast <supratouchgussii@gmail.com>
2022-10-23 14:40:23 +0700

    fix: regis cors

--- apps/regis/src/main.ts
+++ apps/regis/src/main.ts
@@ -1,14 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaClient } from '@prisma/client';
-import 'dotenv/config'
+import 'dotenv/config';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(4000);
-  
+
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
